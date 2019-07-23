32fb14c4d9583c3ec6bf75402fd0d387b6868826
sountstars <1298754741@qq.com>
2019-07-24 00:03:17 +0800

    Delete url切参数.js

--- url切参数.js
+++
@@ -1,9 +0,0 @@
-  //切url？参数
-  queryURLParameter(url) {
-    let regParam = /([^?&=#]+)=?([^?&=#]+)?/ig,
-        obj = {};
-    url.replace(regParam, (...arg) => {
-        obj[arg[1]] = arg[2];
-    });
-    return obj;
-}
